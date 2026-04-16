467990d0a20a8d38d3fc53b4949114607e2774fb
Manish Dewangan <manishdewangan1302@gmail.com>
2026-04-16 20:16:16 +0530

    checking

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -8,27 +8,6 @@ export const isAdmin = async (req, res) =>{
   res.json({success: true, isAdmin: true})
 } 
 
-// API to get dashboard data
-// export const getDashboardData = async (req, res) => {
-//   try {
-//     const bookings = await Booking.find({isPaid: true});
-//     const activeShows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie');
-
-//     const totalUser = await User.countDocuments();
-
-//     const dashboardData = {
-//       totalBookings : booking.length,
-//       totalRevenue : bookings.reduce((acc, bookings) => acc + bookings.amount,0),
-//       activeShows,
-//       totalUser
-//     }
-
-//     res.json({success:true, dashboardData})
-//   } catch (error) {
-//     console.error(error);
-//     res.json({success: false, message : error.message})  
-//   }
-// }
 
 export const getDashboardData = async (req, res) => {
   try {
@@ -58,18 +37,6 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
-
-//API to get all shows
-// export const getAllShows = async (req, res) =>{
-//   try {
-//     const shows = (await Show.find({showDateTime: {$gte : new Date()}}).populate('movie')).toSorted({showDateTime: 1})
-//     res.json({success: true, shows})  
-
-//   } catch (error) {
-//     console.error(error);
-//     res.json({success: false, message : error.message})  
-//   }
-// }
 
 export const getAllShows = async (req, res) => {
   try {
